1b7a7db011a468e3fdc95a83dc2fe4624a4ca5a5
Björn Strömberg <bjorn.stromberg@codic.se>
2021-09-21 11:35:16 +0200

    Debit card TDD iteration

--- C#/v38/Tests/InterfaceTests.cs
+++ C#/v38/Tests/InterfaceTests.cs
@@ -12,7 +12,37 @@ namespace Tests
         [Fact]
         void Test_DebitCard()
         {
+            var card = new DebitCard(1000);
 
+            var paymentMade =  card.TryMakePayment(600);
+            Assert.True(paymentMade);
+
+            paymentMade = card.TryMakePayment(600);
+            Assert.False(paymentMade);
+        }
+    }
+
+    interface IPayer
+    {
+        public bool TryMakePayment(int amount);
+    }
+    class DebitCard : IPayer
+    {
+        private int _balance;
+        public DebitCard(int balance)
+        {
+            _balance = balance;
+        }
+        public bool TryMakePayment(int amount)
+        {
+            bool canPay = amount <= _balance;
+
+            if (canPay)
+            {
+                _balance -= amount;
+            }
+
+            return canPay;
         }
     }
 }
