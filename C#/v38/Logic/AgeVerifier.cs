8de003b46fa33cce94a18626346857aaa900f6ba
Björn Strömberg <bjorn.stromberg@codic.se>
2021-09-21 10:41:54 +0200

    Genom att skicka med DateTime överallt så kan man lösa problemet. Många parametrar att hålla reda på dock!

--- C#/v38/Logic/AgeVerifier.cs
+++ C#/v38/Logic/AgeVerifier.cs
@@ -4,5 +4,21 @@ namespace Logic
 {
     public class AgeVerifier
     {
+        private readonly int _ageLimit;
+
+        public AgeVerifier(int ageLimit)
+        {
+            _ageLimit = ageLimit;
+        }
+
+        public bool Try(Person person, DateTime now)
+        {
+            TimeSpan age = now - person.BirthDate;
+
+            int yearsOld = age.Days / 365;
+            bool ofAge = yearsOld >= _ageLimit;
+
+            return ofAge;
+        }
     }
 }
--- C#/v38/Tests/AgeVerifierTests.cs
+++ C#/v38/Tests/AgeVerifierTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Logic;
 using Xunit;
 
@@ -7,8 +8,55 @@ namespace Tests
     public class AgeVerifierTests
     {
         [Fact]
+        public void Test_MinorNotAllowed()
+        {
+            AgeVerifier verfier = new AgeVerifier(18);
+            Person person = new Person() 
+                {
+                    Name = "Sandra", 
+                    BirthDate = DateTime.Today
+                };
+
+            bool oldEnough = verfier.Try(person, DateTime.Now);
+
+            Assert.False(oldEnough);
+        }
+
+        [Fact]
+        public void Test_AdultAllowed()
+        {
+            AgeVerifier verfier = new AgeVerifier(18);
+            Person person = new Person()
+            {
+                Name = "Sandra",
+                BirthDate = DateTime.Today - TimeSpan.FromDays(365 * 19)
+            };
+
+            bool oldEnough = verfier.Try(person, DateTime.Now);
+
+            Assert.True(oldEnough);
+        }
+
+        [Fact]
         public void Test_ComingOfAge()
         {
+            AgeVerifier verfier = new AgeVerifier(18);
+            Person person = new Person()
+            {
+                Name = "Sandra",
+                BirthDate = DateTime.Today - TimeSpan.FromDays(365 * 17)
+            };
+
+            DateTime now = DateTime.Now;
+
+            bool oldEnough = verfier.Try(person, now);
+            Assert.False(oldEnough);
+
+            // det går ett år
+            now += TimeSpan.FromDays(365 * 1);
+
+            oldEnough = verfier.Try(person, now);
+            Assert.True(oldEnough);
         }
     }
 }
