d346abdd1e198b91e63a0d087e389c62017fab8b
Björn Strömberg <bjorn.stromberg@codic.se>
2021-09-22 11:59:35 +0200

    [Red] för Test_AppointmentsAreSaved

--- C#/v38/Tests/SchedulerTests.cs
+++ C#/v38/Tests/SchedulerTests.cs
@@ -42,5 +42,22 @@ namespace Tests
             Assert.Equal("buy milk", active[0].What);
             Assert.Equal("buy eggs", finished[0].What);
         }
+
+        [Fact]
+        void Test_AppointmentsAreSaved()
+        {
+            var scheduler = new Scheduler();
+
+            scheduler.AddAppointment("buy milk",
+                DateTime.Today + TimeSpan.FromDays(1));
+
+            // starta om scheduler
+            scheduler = new Scheduler();
+
+            var active = scheduler.GetActiveAppointments();
+
+            Assert.Equal(1, active.Count);
+            Assert.Equal("buy milk", active[0].What);
+        }
     }
 }
