9c03c857f816f82a5cc053690b40a1e7e361b25c
Björn Strömberg <bjorn.stromberg@codic.se>
2021-09-22 11:46:24 +0200

    [TDD iteration] Test_CanAddAppointment

---
+++ C#/v38/Logic/Scheduler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Scheduler
+    {
+        private List<Appointment> _appointments;
+
+        public Scheduler()
+        {
+            _appointments = new List<Appointment>();
+        }
+
+        public void AddAppointment(string what, DateTime when)
+        {
+            _appointments.Add(new Appointment()
+            {
+                What = what
+            });
+        }
+
+        public List<Appointment> GetActiveAppointments()
+        {
+            return _appointments;
+        }
+    }
+}
---
+++ C#/v38/Tests/SchedulerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic;
+using Xunit;
+
+namespace Tests
+{
+    public class SchedulerTests
+    {
+        [Fact]
+        void Test_CanAddAppointment()
+        {
+            var scheduler = new Scheduler();
+
+            scheduler.AddAppointment("buy milk", DateTime.Today + TimeSpan.FromDays(1));
+
+            var active = scheduler.GetActiveAppointments();
+
+            Assert.Equal("get milk", active[0].What);
+        }
+    }
+}
