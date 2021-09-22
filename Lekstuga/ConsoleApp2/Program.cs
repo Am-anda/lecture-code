9236bd76b0ef0033c620992d60676f7895ded97c
Björn Strömberg <bjorn.stromberg@codic.se>
2021-09-22 10:54:31 +0200

    En spike för att förstå filer i lekstugan

---
+++ Lekstuga/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            Console.WriteLine(path);
+
+            string adress = Path.Combine(path, "Downloads", "test.txt");
+            Console.WriteLine(adress);
+
+            if (File.Exists(adress))
+            {
+                Console.WriteLine("file exist");
+            }
+            else
+            {
+                Console.WriteLine("file does not exist");
+            }
+
+            string fileName = adress;
+            using (var sr = new StreamReader(fileName))
+            {
+                string content = sr.ReadLine();
+                Console.WriteLine(content);
+            }
+            
+            using (var sw = new StreamWriter(fileName))
+            {
+                sw.WriteLine("Skriv något nytt");
+                sw.WriteLine("Skriv en sak till");
+                sw.WriteLine("Avsluta");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
